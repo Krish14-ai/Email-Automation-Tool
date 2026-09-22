01b44e547e959c6b0ddfa8d3272de5b792f7e7d5
krish14-ai <kkhandelwal292@gmail.com>
2026-09-23 00:38:32 +0530

    Changed name and imported methods

--- src/gmail_auth.py
+++ src/gmail_auth.py
@@ -4,6 +4,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from google.auth.transport.requests import Request
+import datetime
 
 def Create_Service(client_secret_file, api_name, api_version , *scopes):
     print(client_secret_file, api_name, api_version, scopes, sep='-')
@@ -38,3 +39,10 @@ def Create_Service(client_secret_file, api_name, api_version , *scopes):
         return service
     except Exception as e : 
         print("unable to connect.")
+        print(e)
+        return None
+    
+
+def convert_to_REC_datetime(year = 1900, month =1, day = 1, hour = 0, minute = 0):
+    dt  = datetime.datetime(year, month , day , hour, minute ,0).isoformat() +'Z' 
+    return dt
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from src.gmail_auth import Create_Service 
+import base64
+from email.mime.multipart import MIMEMultipart
+
+
