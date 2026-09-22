496bbd482a642dab25fef9e40efa7d6fac090d4c
krish14-ai <kkhandelwal292@gmail.com>
2026-09-23 01:54:40 +0530

    partially working

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
-from src.gmail_auth import Create_Service 
+from gmail_auth import Create_Service 
 import base64
 from email.mime.multipart import MIMEMultipart
 
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_NAME = 'gmail'
+API_VERSION= 'v1'
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
