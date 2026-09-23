5ce8cc0eec97ea9c458511f22c0c829dc7e6f07b
krish14-ai <kkhandelwal292@gmail.com>
2026-09-24 01:31:42 +0530

    Done for today

--- src/main.py
+++ src/main.py
@@ -1,49 +1,42 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-from src.gmail_auth import Create_Service 
-=======
-from gmail_auth import Create_Service 
->>>>>>> origin/main
-import base64
-from email.mime.multipart import MIMEMultipart
-
-CLIENT_SECRET_FILE = 'client_secret.json'
-API_NAME = 'gmail'
-API_VERSION= 'v1'
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
-
-<<<<<<< HEAD
-=======
-from gmail_auth import Create_Service 
+from gmail_auth import Create_Service
 import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
 import pandas as pd
 
 CLIENT_SECRET_FILE = 'client_secret.json'
 API_NAME = 'gmail'
-API_VERSION= 'v1'
+API_VERSION = 'v1'
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
 emailmsg = 'hlo bruh'
-mimeMessage = MIMEMultipart()
 
-df = pd.read_csv(r"C:\Users\Krish\Downloads\Email-Automation-Tool\mails\data\recipients.csv")
+df = pd.read_csv(
+    r"C:\Users\Krish\Downloads\Email-Automation-Tool\mails\data\recipients.csv"
+)
+
 print(df)
 
-# mimeMessage['to'] = 'architsv027@gmail.com'
-# mimeMessage['subject'] = "greetings"
-# mimeMessage.attach(MIMEText(emailmsg,'plan'))
+for mail in df['mail']:
 
-# raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+    mimeMessage = MIMEMultipart()
 
-# message = service.users().messages().send(userId = 'me',body = {'raw' : raw_string}).execute()
+    mimeMessage['to'] = mail
+    mimeMessage['subject'] = "greetings"
 
-# print(message)
->>>>>>> apple
-=======
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
->>>>>>> origin/main
+    mimeMessage.attach(
+        MIMEText(emailmsg, 'plain')
+    )
+
+    raw_string = base64.urlsafe_b64encode(
+        mimeMessage.as_bytes()
+    ).decode()
+
+    message = service.users().messages().send(
+        userId='me',
+        body={'raw': raw_string}
+    ).execute()
+
+    print(f"Email sent to {mail}")
