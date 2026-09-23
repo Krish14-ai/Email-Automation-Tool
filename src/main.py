de7932787cdb4517a78dc355f41323ca41321f68
krish14-ai <kkhandelwal292@gmail.com>
2026-09-24 00:06:17 +0530

    Solved the problem and the code is working

--- src/main.py
+++ src/main.py
@@ -18,6 +18,6 @@ mimeMessage.attach(MIMEText(emailmsg,'plan'))
 
 raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
 
-message = service.users().messages().send(userID = 'me',body = {'raw' : raw_string}).execute()
+message = service.users().messages().send(userId = 'me',body = {'raw' : raw_string}).execute()
 
 print(message)
