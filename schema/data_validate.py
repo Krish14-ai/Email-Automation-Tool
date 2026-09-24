97c9d6f8366cab7c657304d30c5663b6f573850f
krish14-ai <kkhandelwal292@gmail.com>
2026-09-24 12:45:07 +0530

    validating mails

---
+++ schema/data_validate.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+
+def get_all_mails():
+    data = pd.read_csv(r"C:\Users\Krish\Downloads\Email-Automation-Tool\mails\data\recipients.csv")
+    
+    return data["mail"].tolist()
--- schema/validate.py
+++ schema/validate.py
@@ -1,2 +1,15 @@
 import pydantic
-from fastapi
+from fastapi import FastAPI
+from schema.data_validate import get_all_mails 
+
+app = FastAPI()
+
+@app.get("/")
+def enter():
+    return {"message" : "hello"}
+
+@app.get("/mails")
+def get_mails():
+    mails = get_all_mails()
+    
+    return {"all mails" : mails}
