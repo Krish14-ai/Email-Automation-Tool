f297297dedb8a07befb45867c3f70cefd842889e
krish14-ai <kkhandelwal292@gmail.com>
2026-09-25 23:44:10 +0530

    Validated email and UserId

--- schema/data_validate.py
+++ schema/data_validate.py
@@ -1,20 +1,47 @@
-import pandas as pd 
-from pydantic import BaseModel, field_validator, Field, EmailStr
+from pydantic import BaseModel, field_validator, Field, EmailStr, ValidationInfo
 from typing import Annotated
 
-class mails(BaseModel):
-    name = Annotated[str, Field(..., description= "please Enter your name", min_length= 2, max_length=100)]
-    
-    user_id = Annotated[int, Field(..., ge= 0,description= "please enter your user id",min_length= 1)]
 
-    mail = Annotated[EmailStr, Field(..., description= "Please enter your gmail")]
+class Mails(BaseModel):
+
+    name: Annotated[
+        str,
+        Field(
+            ...,
+            description="Please enter your name",
+            min_length=2,
+            max_length=100
+        )
+    ]
+
+    user_id: Annotated[
+        str,
+        Field(
+            ...,
+            pattern=r"^\d{3}$",
+            description="User Id must be of 3 digits"
+        )
+    ]
+
+    mail: Annotated[
+        EmailStr,
+        Field(
+            ...,
+            description="Please enter your email"
+        )
+    ]
+
+    @field_validator("mail", mode="after")
+    @classmethod
+    def validate_mail(cls, data, info: ValidationInfo):
+
+        user_id = info.data.get("user_id")
+
+        if "-" in str(data):
+            raise ValueError(
+                f"Invalid Gmail found at user {user_id}"
+            )
+
+        return data
     
     
-    @field_validator("mail", mode = "after")
-    def validate_mail(cls, data):
-        
-        for i, row in enumerate(data.itertuples(index=False), start=1):
-            user_id = row.user_id
-            email = row.mail
-            if "-" in email : 
-                raise ValueError(f"invalid gamil found at user {user_id}")            
