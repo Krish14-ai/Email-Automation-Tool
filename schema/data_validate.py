62edba90f2e4ea971782b925aaba26225854adf5
krish14-ai <kkhandelwal292@gmail.com>
2026-09-25 23:24:38 +0530

    almost there

--- schema/data_validate.py
+++ schema/data_validate.py
@@ -1,8 +1,11 @@
 import pandas as pd 
-from pydantic import BaseModel, field_validator, Field
+from pydantic import BaseModel, field_validator, Field, EmailStr
 from typing import Annotated
 
 class mails(BaseModel):
     name = Annotated[str, Field(..., description= "please Enter your name", min_length= 2, max_length=100)]
     
+    user_id = Annotated[int, Field(..., ge= 0,description= "please enter your user id",min_length= 1)]
+
+    mail = Annotated[EmailStr, Field(..., description= "Please enter your gmail")]
     
